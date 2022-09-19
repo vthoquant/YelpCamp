7213db5a31facfaf8b30d118821af568f99fe93c
vthoquant <38273791+vthoquant@users.noreply.github.com>
2022-09-19 18:06:29 +0530

    basic styling using bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -15,6 +16,7 @@ async function mongooseConnect() {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
@@ -32,7 +34,7 @@ app.get('/campgrounds', async(req, res) => {
 app.post('/campgrounds', async(req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect('/campgrounds');
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/new', (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i=0; i<50; i++) {
         const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, nam. Quibusdam fuga natus aut possimus soluta eius autem totam laudantium, esse eligendi doloribus debitis consectetur molestiae ullam facere, eveniet impedit?',
+            price
         });
 
         await camp.save();
